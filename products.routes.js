c82e0cc39a04137f9ebd7f9cfc713cb03b9455e0
carolyne-wanjiru <shirogithenduka@gmail.com>
2024-06-26 19:47:21 +0300

    will create a new product inside the databse

--- products.routes.js
+++ products.routes.js
@@ -81,40 +81,40 @@ const id=req.params.id;
 
 })
 
-// router.post("/", async(req,res)=>{
-//            const{productTitle,
-//             productDescription,
-//             productCost,
-//             onOffer}=req.body
-//             if (!productTitle ||!productDescription|| !productCost || typeof onOffer === 'undefined') {
-//     return res.status(400).json({ success: false, message: "All fields are required" });
-//   }
-//    try{
-//     const products=await prisma.product.create({
+router.post("/", async(req,res)=>{
+           const{productTitle,
+            productDescription,
+            productCost,
+            onOffer}=req.body
+            if (!productTitle ||!productDescription|| !productCost || typeof onOffer === 'undefined') {
+    return res.status(400).json({ success: false, message: "All fields are required" });
+  }
+   try{
+    const products=await prisma.product.create({
 
-//         data:{
-//             productTitle,
-//             productDescription,
-//             productCost,
-//             onOffer
-//         },
-//         select:{
+        data:{
+            productTitle,
+            productDescription,
+            productCost,
+            onOffer
+        },
+        select:{
           
-//           productTitle:true,
-//             productDescription:true,
-//             productCost:true,
-//             onOffer:true  
-//         }
+          productTitle:true,
+            productDescription:true,
+            productCost:true,
+            onOffer:true  
+        }
       
-//     })
-//       res.status(200).json({message:"Product created successfully" ,products})
-//   }
-//   catch(err){
-//      res.status(500).json({message:"error has occured"})
+    })
+      res.status(200).json({message:"Product created successfully" ,products})
+  }
+  catch(err){
+     res.status(500).json({message:"error has occured"})
   
-//   }
+  }
 
-// })
+})
 
 // router.patch("/:id", async(req,res)=>{
 //    const id = req.params.id;
